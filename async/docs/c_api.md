6c3b20061a0796eabf78de88303fde7627e7e8ab
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-12-10 18:53:08 +0200

    + php async

---
+++ async/docs/c_api.md
@@ -0,0 +1,22 @@
+# C-level API
+
+## Await
+
+```c++
+
+typedef struct _async_future_s {
+    int state;
+    zval *result;
+} _async_future_t;
+
+```
+
+```c++
+
+typedef struct _async_await_context_s {
+    
+    
+    
+} async_await_context_t;
+
+```
--- async/php_async.h
+++ async/php_async.h
@@ -5,6 +5,84 @@
 #ifndef PHP_ASYNC_H
 #define PHP_ASYNC_H
 
+#include "php.h"
+#include "zend_types.h"
+#include "zend_atomic.h"
+#include "uv.h"
+
+//
+// Structures block start
+//
+
+typedef struct _async_future_s async_future;
+typedef struct _async_deferred_resume_s async_deferred_resume;
+typedef struct _async_await_context_s async_await_context;
+typedef struct _async_event_handler_s async_event_handler;
+
+// Future statuses
+typedef enum {
+    ASYNC_FUTURE_PENDING     = 0,
+    ASYNC_FUTURE_RESOLVED    = 1,
+    ASYNC_FUTURE_REJECTED    = 2,
+    ASYNC_FUTURE_CANCELLED   = 3
+} ASYNC_FUTURE_STATUS;
+
+// Future structure
+struct _async_future_s {
+    zend_atomic_int status;
+    zval *result;
+};
+
+// DeferredResume structure
+struct _async_deferred_resume_s {
+    zend_atomic_int status;
+    zend_fiber *fiber;
+    zval *value;
+};
+
+//
+// EventHandler structure
+// This structure is used to handle events in the event loop.
+//
+
+typedef enum {
+    ASYNC_CONTEXT_READY    = 0,
+    ASYNC_CONTEXT_USED     = 1,
+    ASYNC_CONTEXT_DISPOSED = 2,
+} ASYNC_CONTEXT_STATUS;
+
+typedef void (*async_event_handler_method)(async_event_handler *event_handler);
+
+struct _async_event_handler_s {
+    async_event_handler_method callback;
+    async_event_handler_method dispose;
+    zval *event_descriptor;
+    async_await_context *context;
+    uv_handle_t uv_handle;
+};
+
+//
+// AwaitContext structure
+// This structure is used to handle the context of the await.
+//
+
+typedef void (*async_context_method)(async_await_context *await_context);
+
+struct _async_await_context_s {
+    zend_atomic_int status;
+    async_deferred_resume *deferred_resume;
+    zval *context;
+    async_context_method apply;
+    async_context_method reset;
+    async_context_method dispose;
+    size_t event_handler_count;
+    async_event_handler event_handlers[];
+};
+
+//
+// Structures block end
+//
+
 // #define ASYNC_IS_FIBER !EG(active_fiber)
 #define ASYNC_IS_FIBER true
 
