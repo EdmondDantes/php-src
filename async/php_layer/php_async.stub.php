74f4a2f18f238ce7d58d75f346baf1f10275bb6e
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-12-10 20:21:31 +0200

    % refactoring directories

--- async/php_layer/php_async.stub.php
+++ async/php_layer/php_async.stub.php
@@ -4,9 +4,20 @@
 
 namespace Async;
 
+enum FutureStatus: int
+{
+    case PENDING    = 0;
+    case RESOLVED   = 1;
+    case REJECTED   = 2;
+    case CANCELLED  = 3;
+}
+
+/**
+ * Describes an object that provides a deferred result.
+ */
 interface FutureInterface
 {
-    public function getStatus(): int;
+    public function getStatus(): FutureStatus;
 
     public function isReady(): bool;
 
@@ -17,11 +28,18 @@ interface FutureInterface
     public function getError(): ?\Throwable;
 }
 
+/**
+ * The `CancellationInterface` interface is used to ignore the result of an awaitable object.
+ */
 interface CancellationInterface
 {
     public function cancel(): void;
 }
 
+/**
+ * The `EventDescriptorInterface` interface is used to implement an event descriptor
+ * and handler that is invoked when the event occurs.
+ */
 interface EventDescriptorInterface extends CancellationInterface
 {
     /**
@@ -30,6 +48,9 @@ interface EventDescriptorInterface extends CancellationInterface
     public function onRegistered(callable $disposeCallback): void;
 }
 
+/**
+ * The `AwaitableInterface` interface is used to implement an awaitable object.
+ */
 interface AwaitableInterface
 {
     /**
@@ -47,6 +68,10 @@ interface AwaitableInterface
     public function getWaitingEvents(): array;
 }
 
+/**
+ * The `CompletionPublisherInterface` interface is used to implement a completion publisher
+ * that notifies subscribers when the operation is completed.
+ */
 interface CompletionPublisherInterface
 {
     /**
@@ -74,6 +99,9 @@ interface CompletionPublisherInterface
     public function onFinally(callable $onFinally): void;
 }
 
+/**
+ * The `ThenInterface` interface is used to register callbacks for completion events.
+ */
 interface ThenInterface
 {
     /**
@@ -95,6 +123,9 @@ interface ThenInterface
     public function thenReject(DeferredInterface ...$objects): static;
 }
 
+/**
+ * An interface for objects whose state can be changed once from undefined to resolve.
+ */
 interface DeferredInterface extends FutureInterface,
     CancellationInterface,
     CompletionPublisherInterface,
@@ -125,21 +156,8 @@ interface DeferredInterface extends FutureInterface,
     public function getFuture(): FutureInterface;
 }
 
-
-abstract class DeferredAbstract implements DeferredInterface
+abstract class CompletionPublisherAbstract implements CompletionPublisherInterface
 {
-    public function getStatus(): int {}
-
-    public function isReady(): bool {}
-
-    public function isFailed(): bool {}
-
-    public function getResult(): mixed {}
-
-    public function getError(): ?\Throwable {}
-
-    public function cancel(): void {}
-
     public function onSuccess(callable $onFulfilled): void {}
 
     public function onError(callable $onRejected): void {}
@@ -152,13 +170,89 @@ abstract class DeferredAbstract implements DeferredInterface
 
     public function thenReject(DeferredInterface ...$objects): static {}
 
+    protected function invokeCompletionHandlers(): void {}
+}
+
+/**
+ * The Base class for implementing the DeferredInterface.
+ * It can be used to create a custom Deferred object.
+ */
+abstract class DeferredAbstract extends CompletionPublisherAbstract implements DeferredInterface
+{
+    public function getStatus(): FutureStatus {}
+
+    public function isReady(): bool {}
+
+    public function isFailed(): bool {}
+
+    public function getResult(): mixed {}
+
+    public function getError(): ?\Throwable {}
+
+    public function cancel(): void {}
+
     public function resolve(mixed $value): void {}
 
     public function reject(\Throwable $error): void {}
 
     public function getFuture(): FutureInterface {}
+
+    /**
+     * Internal handler for the Deferred object.
+     * This method is called when the Deferred object is resolved or rejected.
+     */
+    abstract protected function internalHandler(): void;
 }
 
+/**
+ * The key class of the library for implementing `Fiber` switching is the `DeferredResume` class.
+ */
 final class DeferredResume extends DeferredAbstract
 {
+    protected function internalHandler(): void {}
+}
+
+/**
+ *
+ */
+class InputOutputEvent extends CompletionPublisherAbstract implements EventDescriptorInterface
+{
+    public function cancel(): void {}
+
+    public function onRegistered(callable $disposeCallback): void {}
+}
+
+final class TimerEvent extends CompletionPublisherAbstract implements EventDescriptorInterface
+{
+    public function cancel(): void {}
+
+    public function onRegistered(callable $disposeCallback): void {}
+}
+
+final class SignalEvent extends CompletionPublisherAbstract implements EventDescriptorInterface
+{
+    public function cancel(): void {}
+
+    public function onRegistered(callable $disposeCallback): void {}
+}
+
+final class FileSystemEvent extends CompletionPublisherAbstract implements EventDescriptorInterface
+{
+    public function cancel(): void {}
+
+    public function onRegistered(callable $disposeCallback): void {}
+}
+
+final class ProcessEvent extends CompletionPublisherAbstract implements EventDescriptorInterface
+{
+    public function cancel(): void {}
+
+    public function onRegistered(callable $disposeCallback): void {}
+}
+
+final class ThreadEvent extends CompletionPublisherAbstract implements EventDescriptorInterface
+{
+    public function cancel(): void {}
+
+    public function onRegistered(callable $disposeCallback): void {}
 }
--- async/php_layer/php_async_arginfo.h
+++ async/php_layer/php_async_arginfo.h
@@ -1,7 +1,7 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: d782c465f885682f2061e5089bce1d8ec356abf4 */
+ * Stub hash: dbebb49051294b48bc37eed812a0532d773440ed */
 
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Async_FutureInterface_getStatus, 0, 0, IS_LONG, 0)
+ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(arginfo_class_Async_FutureInterface_getStatus, 0, 0, Async\\FutureStatus, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Async_FutureInterface_isReady, 0, 0, _IS_BOOL, 0)
@@ -61,6 +61,20 @@ ZEND_END_ARG_INFO()
 ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(arginfo_class_Async_DeferredInterface_getFuture, 0, 0, Async\\FutureInterface, 0)
 ZEND_END_ARG_INFO()
 
+#define arginfo_class_Async_CompletionPublisherAbstract_onSuccess arginfo_class_Async_CompletionPublisherInterface_onSuccess
+
+#define arginfo_class_Async_CompletionPublisherAbstract_onError arginfo_class_Async_CompletionPublisherInterface_onError
+
+#define arginfo_class_Async_CompletionPublisherAbstract_onFinally arginfo_class_Async_CompletionPublisherInterface_onFinally
+
+#define arginfo_class_Async_CompletionPublisherAbstract_thenIgnore arginfo_class_Async_ThenInterface_thenIgnore
+
+#define arginfo_class_Async_CompletionPublisherAbstract_thenResolve arginfo_class_Async_ThenInterface_thenResolve
+
+#define arginfo_class_Async_CompletionPublisherAbstract_thenReject arginfo_class_Async_ThenInterface_thenResolve
+
+#define arginfo_class_Async_CompletionPublisherAbstract_invokeCompletionHandlers arginfo_class_Async_CancellationInterface_cancel
+
 #define arginfo_class_Async_DeferredAbstract_getStatus arginfo_class_Async_FutureInterface_getStatus
 
 #define arginfo_class_Async_DeferredAbstract_isReady arginfo_class_Async_FutureInterface_isReady
@@ -73,39 +87,69 @@ ZEND_END_ARG_INFO()
 
 #define arginfo_class_Async_DeferredAbstract_cancel arginfo_class_Async_CancellationInterface_cancel
 
-#define arginfo_class_Async_DeferredAbstract_onSuccess arginfo_class_Async_CompletionPublisherInterface_onSuccess
-
-#define arginfo_class_Async_DeferredAbstract_onError arginfo_class_Async_CompletionPublisherInterface_onError
-
-#define arginfo_class_Async_DeferredAbstract_onFinally arginfo_class_Async_CompletionPublisherInterface_onFinally
-
-#define arginfo_class_Async_DeferredAbstract_thenIgnore arginfo_class_Async_ThenInterface_thenIgnore
-
-#define arginfo_class_Async_DeferredAbstract_thenResolve arginfo_class_Async_ThenInterface_thenResolve
-
-#define arginfo_class_Async_DeferredAbstract_thenReject arginfo_class_Async_ThenInterface_thenResolve
-
 #define arginfo_class_Async_DeferredAbstract_resolve arginfo_class_Async_DeferredInterface_resolve
 
 #define arginfo_class_Async_DeferredAbstract_reject arginfo_class_Async_DeferredInterface_reject
 
 #define arginfo_class_Async_DeferredAbstract_getFuture arginfo_class_Async_DeferredInterface_getFuture
 
+#define arginfo_class_Async_DeferredAbstract_internalHandler arginfo_class_Async_CancellationInterface_cancel
+
+#define arginfo_class_Async_DeferredResume_internalHandler arginfo_class_Async_CancellationInterface_cancel
+
+#define arginfo_class_Async_InputOutputEvent_cancel arginfo_class_Async_CancellationInterface_cancel
+
+#define arginfo_class_Async_InputOutputEvent_onRegistered arginfo_class_Async_EventDescriptorInterface_onRegistered
+
+#define arginfo_class_Async_TimerEvent_cancel arginfo_class_Async_CancellationInterface_cancel
+
+#define arginfo_class_Async_TimerEvent_onRegistered arginfo_class_Async_EventDescriptorInterface_onRegistered
+
+#define arginfo_class_Async_SignalEvent_cancel arginfo_class_Async_CancellationInterface_cancel
+
+#define arginfo_class_Async_SignalEvent_onRegistered arginfo_class_Async_EventDescriptorInterface_onRegistered
+
+#define arginfo_class_Async_FileSystemEvent_cancel arginfo_class_Async_CancellationInterface_cancel
+
+#define arginfo_class_Async_FileSystemEvent_onRegistered arginfo_class_Async_EventDescriptorInterface_onRegistered
+
+#define arginfo_class_Async_ProcessEvent_cancel arginfo_class_Async_CancellationInterface_cancel
+
+#define arginfo_class_Async_ProcessEvent_onRegistered arginfo_class_Async_EventDescriptorInterface_onRegistered
+
+#define arginfo_class_Async_ThreadEvent_cancel arginfo_class_Async_CancellationInterface_cancel
+
+#define arginfo_class_Async_ThreadEvent_onRegistered arginfo_class_Async_EventDescriptorInterface_onRegistered
+
+ZEND_METHOD(Async_CompletionPublisherAbstract, onSuccess);
+ZEND_METHOD(Async_CompletionPublisherAbstract, onError);
+ZEND_METHOD(Async_CompletionPublisherAbstract, onFinally);
+ZEND_METHOD(Async_CompletionPublisherAbstract, thenIgnore);
+ZEND_METHOD(Async_CompletionPublisherAbstract, thenResolve);
+ZEND_METHOD(Async_CompletionPublisherAbstract, thenReject);
+ZEND_METHOD(Async_CompletionPublisherAbstract, invokeCompletionHandlers);
 ZEND_METHOD(Async_DeferredAbstract, getStatus);
 ZEND_METHOD(Async_DeferredAbstract, isReady);
 ZEND_METHOD(Async_DeferredAbstract, isFailed);
 ZEND_METHOD(Async_DeferredAbstract, getResult);
 ZEND_METHOD(Async_DeferredAbstract, getError);
 ZEND_METHOD(Async_DeferredAbstract, cancel);
-ZEND_METHOD(Async_DeferredAbstract, onSuccess);
-ZEND_METHOD(Async_DeferredAbstract, onError);
-ZEND_METHOD(Async_DeferredAbstract, onFinally);
-ZEND_METHOD(Async_DeferredAbstract, thenIgnore);
-ZEND_METHOD(Async_DeferredAbstract, thenResolve);
-ZEND_METHOD(Async_DeferredAbstract, thenReject);
 ZEND_METHOD(Async_DeferredAbstract, resolve);
 ZEND_METHOD(Async_DeferredAbstract, reject);
 ZEND_METHOD(Async_DeferredAbstract, getFuture);
+ZEND_METHOD(Async_DeferredResume, internalHandler);
+ZEND_METHOD(Async_InputOutputEvent, cancel);
+ZEND_METHOD(Async_InputOutputEvent, onRegistered);
+ZEND_METHOD(Async_TimerEvent, cancel);
+ZEND_METHOD(Async_TimerEvent, onRegistered);
+ZEND_METHOD(Async_SignalEvent, cancel);
+ZEND_METHOD(Async_SignalEvent, onRegistered);
+ZEND_METHOD(Async_FileSystemEvent, cancel);
+ZEND_METHOD(Async_FileSystemEvent, onRegistered);
+ZEND_METHOD(Async_ProcessEvent, cancel);
+ZEND_METHOD(Async_ProcessEvent, onRegistered);
+ZEND_METHOD(Async_ThreadEvent, cancel);
+ZEND_METHOD(Async_ThreadEvent, onRegistered);
 
 static const zend_function_entry class_Async_FutureInterface_methods[] = {
 	ZEND_RAW_FENTRY("getStatus", NULL, arginfo_class_Async_FutureInterface_getStatus, ZEND_ACC_PUBLIC|ZEND_ACC_ABSTRACT, NULL, NULL)
@@ -153,6 +197,17 @@ static const zend_function_entry class_Async_DeferredInterface_methods[] = {
 	ZEND_FE_END
 };
 
+static const zend_function_entry class_Async_CompletionPublisherAbstract_methods[] = {
+	ZEND_ME(Async_CompletionPublisherAbstract, onSuccess, arginfo_class_Async_CompletionPublisherAbstract_onSuccess, ZEND_ACC_PUBLIC)
+	ZEND_ME(Async_CompletionPublisherAbstract, onError, arginfo_class_Async_CompletionPublisherAbstract_onError, ZEND_ACC_PUBLIC)
+	ZEND_ME(Async_CompletionPublisherAbstract, onFinally, arginfo_class_Async_CompletionPublisherAbstract_onFinally, ZEND_ACC_PUBLIC)
+	ZEND_ME(Async_CompletionPublisherAbstract, thenIgnore, arginfo_class_Async_CompletionPublisherAbstract_thenIgnore, ZEND_ACC_PUBLIC)
+	ZEND_ME(Async_CompletionPublisherAbstract, thenResolve, arginfo_class_Async_CompletionPublisherAbstract_thenResolve, ZEND_ACC_PUBLIC)
+	ZEND_ME(Async_CompletionPublisherAbstract, thenReject, arginfo_class_Async_CompletionPublisherAbstract_thenReject, ZEND_ACC_PUBLIC)
+	ZEND_ME(Async_CompletionPublisherAbstract, invokeCompletionHandlers, arginfo_class_Async_CompletionPublisherAbstract_invokeCompletionHandlers, ZEND_ACC_PROTECTED)
+	ZEND_FE_END
+};
+
 static const zend_function_entry class_Async_DeferredAbstract_methods[] = {
 	ZEND_ME(Async_DeferredAbstract, getStatus, arginfo_class_Async_DeferredAbstract_getStatus, ZEND_ACC_PUBLIC)
 	ZEND_ME(Async_DeferredAbstract, isReady, arginfo_class_Async_DeferredAbstract_isReady, ZEND_ACC_PUBLIC)
@@ -160,17 +215,76 @@ static const zend_function_entry class_Async_DeferredAbstract_methods[] = {
 	ZEND_ME(Async_DeferredAbstract, getResult, arginfo_class_Async_DeferredAbstract_getResult, ZEND_ACC_PUBLIC)
 	ZEND_ME(Async_DeferredAbstract, getError, arginfo_class_Async_DeferredAbstract_getError, ZEND_ACC_PUBLIC)
 	ZEND_ME(Async_DeferredAbstract, cancel, arginfo_class_Async_DeferredAbstract_cancel, ZEND_ACC_PUBLIC)
-	ZEND_ME(Async_DeferredAbstract, onSuccess, arginfo_class_Async_DeferredAbstract_onSuccess, ZEND_ACC_PUBLIC)
-	ZEND_ME(Async_DeferredAbstract, onError, arginfo_class_Async_DeferredAbstract_onError, ZEND_ACC_PUBLIC)
-	ZEND_ME(Async_DeferredAbstract, onFinally, arginfo_class_Async_DeferredAbstract_onFinally, ZEND_ACC_PUBLIC)
-	ZEND_ME(Async_DeferredAbstract, thenIgnore, arginfo_class_Async_DeferredAbstract_thenIgnore, ZEND_ACC_PUBLIC)
-	ZEND_ME(Async_DeferredAbstract, thenResolve, arginfo_class_Async_DeferredAbstract_thenResolve, ZEND_ACC_PUBLIC)
-	ZEND_ME(Async_DeferredAbstract, thenReject, arginfo_class_Async_DeferredAbstract_thenReject, ZEND_ACC_PUBLIC)
 	ZEND_ME(Async_DeferredAbstract, resolve, arginfo_class_Async_DeferredAbstract_resolve, ZEND_ACC_PUBLIC)
 	ZEND_ME(Async_DeferredAbstract, reject, arginfo_class_Async_DeferredAbstract_reject, ZEND_ACC_PUBLIC)
 	ZEND_ME(Async_DeferredAbstract, getFuture, arginfo_class_Async_DeferredAbstract_getFuture, ZEND_ACC_PUBLIC)
+	ZEND_RAW_FENTRY("internalHandler", NULL, arginfo_class_Async_DeferredAbstract_internalHandler, ZEND_ACC_PROTECTED|ZEND_ACC_ABSTRACT, NULL, NULL)
 	ZEND_FE_END
 };
+
+static const zend_function_entry class_Async_DeferredResume_methods[] = {
+	ZEND_ME(Async_DeferredResume, internalHandler, arginfo_class_Async_DeferredResume_internalHandler, ZEND_ACC_PROTECTED)
+	ZEND_FE_END
+};
+
+static const zend_function_entry class_Async_InputOutputEvent_methods[] = {
+	ZEND_ME(Async_InputOutputEvent, cancel, arginfo_class_Async_InputOutputEvent_cancel, ZEND_ACC_PUBLIC)
+	ZEND_ME(Async_InputOutputEvent, onRegistered, arginfo_class_Async_InputOutputEvent_onRegistered, ZEND_ACC_PUBLIC)
+	ZEND_FE_END
+};
+
+static const zend_function_entry class_Async_TimerEvent_methods[] = {
+	ZEND_ME(Async_TimerEvent, cancel, arginfo_class_Async_TimerEvent_cancel, ZEND_ACC_PUBLIC)
+	ZEND_ME(Async_TimerEvent, onRegistered, arginfo_class_Async_TimerEvent_onRegistered, ZEND_ACC_PUBLIC)
+	ZEND_FE_END
+};
+
+static const zend_function_entry class_Async_SignalEvent_methods[] = {
+	ZEND_ME(Async_SignalEvent, cancel, arginfo_class_Async_SignalEvent_cancel, ZEND_ACC_PUBLIC)
+	ZEND_ME(Async_SignalEvent, onRegistered, arginfo_class_Async_SignalEvent_onRegistered, ZEND_ACC_PUBLIC)
+	ZEND_FE_END
+};
+
+static const zend_function_entry class_Async_FileSystemEvent_methods[] = {
+	ZEND_ME(Async_FileSystemEvent, cancel, arginfo_class_Async_FileSystemEvent_cancel, ZEND_ACC_PUBLIC)
+	ZEND_ME(Async_FileSystemEvent, onRegistered, arginfo_class_Async_FileSystemEvent_onRegistered, ZEND_ACC_PUBLIC)
+	ZEND_FE_END
+};
+
+static const zend_function_entry class_Async_ProcessEvent_methods[] = {
+	ZEND_ME(Async_ProcessEvent, cancel, arginfo_class_Async_ProcessEvent_cancel, ZEND_ACC_PUBLIC)
+	ZEND_ME(Async_ProcessEvent, onRegistered, arginfo_class_Async_ProcessEvent_onRegistered, ZEND_ACC_PUBLIC)
+	ZEND_FE_END
+};
+
+static const zend_function_entry class_Async_ThreadEvent_methods[] = {
+	ZEND_ME(Async_ThreadEvent, cancel, arginfo_class_Async_ThreadEvent_cancel, ZEND_ACC_PUBLIC)
+	ZEND_ME(Async_ThreadEvent, onRegistered, arginfo_class_Async_ThreadEvent_onRegistered, ZEND_ACC_PUBLIC)
+	ZEND_FE_END
+};
+
+static zend_class_entry *register_class_Async_FutureStatus(void)
+{
+	zend_class_entry *class_entry = zend_register_internal_enum("Async\\FutureStatus", IS_LONG, NULL);
+
+	zval enum_case_PENDING_value;
+	ZVAL_LONG(&enum_case_PENDING_value, 0);
+	zend_enum_add_case_cstr(class_entry, "PENDING", &enum_case_PENDING_value);
+
+	zval enum_case_RESOLVED_value;
+	ZVAL_LONG(&enum_case_RESOLVED_value, 1);
+	zend_enum_add_case_cstr(class_entry, "RESOLVED", &enum_case_RESOLVED_value);
+
+	zval enum_case_REJECTED_value;
+	ZVAL_LONG(&enum_case_REJECTED_value, 2);
+	zend_enum_add_case_cstr(class_entry, "REJECTED", &enum_case_REJECTED_value);
+
+	zval enum_case_CANCELLED_value;
+	ZVAL_LONG(&enum_case_CANCELLED_value, 3);
+	zend_enum_add_case_cstr(class_entry, "CANCELLED", &enum_case_CANCELLED_value);
+
+	return class_entry;
+}
 
 static zend_class_entry *register_class_Async_FutureInterface(void)
 {
@@ -244,12 +358,23 @@ static zend_class_entry *register_class_Async_DeferredInterface(zend_class_entry
 	return class_entry;
 }
 
-static zend_class_entry *register_class_Async_DeferredAbstract(zend_class_entry *class_entry_Async_DeferredInterface)
+static zend_class_entry *register_class_Async_CompletionPublisherAbstract(zend_class_entry *class_entry_Async_CompletionPublisherInterface)
+{
+	zend_class_entry ce, *class_entry;
+
+	INIT_NS_CLASS_ENTRY(ce, "Async", "CompletionPublisherAbstract", class_Async_CompletionPublisherAbstract_methods);
+	class_entry = zend_register_internal_class_with_flags(&ce, NULL, ZEND_ACC_ABSTRACT);
+	zend_class_implements(class_entry, 1, class_entry_Async_CompletionPublisherInterface);
+
+	return class_entry;
+}
+
+static zend_class_entry *register_class_Async_DeferredAbstract(zend_class_entry *class_entry_Async_CompletionPublisherAbstract, zend_class_entry *class_entry_Async_DeferredInterface)
 {
 	zend_class_entry ce, *class_entry;
 
 	INIT_NS_CLASS_ENTRY(ce, "Async", "DeferredAbstract", class_Async_DeferredAbstract_methods);
-	class_entry = zend_register_internal_class_with_flags(&ce, NULL, ZEND_ACC_ABSTRACT);
+	class_entry = zend_register_internal_class_with_flags(&ce, class_entry_Async_CompletionPublisherAbstract, ZEND_ACC_ABSTRACT);
 	zend_class_implements(class_entry, 1, class_entry_Async_DeferredInterface);
 
 	return class_entry;
@@ -259,8 +384,74 @@ static zend_class_entry *register_class_Async_DeferredResume(zend_class_entry *c
 {
 	zend_class_entry ce, *class_entry;
 
-	INIT_NS_CLASS_ENTRY(ce, "Async", "DeferredResume", NULL);
+	INIT_NS_CLASS_ENTRY(ce, "Async", "DeferredResume", class_Async_DeferredResume_methods);
 	class_entry = zend_register_internal_class_with_flags(&ce, class_entry_Async_DeferredAbstract, ZEND_ACC_FINAL);
+
+	return class_entry;
+}
+
+static zend_class_entry *register_class_Async_InputOutputEvent(zend_class_entry *class_entry_Async_CompletionPublisherAbstract, zend_class_entry *class_entry_Async_EventDescriptorInterface)
+{
+	zend_class_entry ce, *class_entry;
+
+	INIT_NS_CLASS_ENTRY(ce, "Async", "InputOutputEvent", class_Async_InputOutputEvent_methods);
+	class_entry = zend_register_internal_class_with_flags(&ce, class_entry_Async_CompletionPublisherAbstract, 0);
+	zend_class_implements(class_entry, 1, class_entry_Async_EventDescriptorInterface);
+
+	return class_entry;
+}
+
+static zend_class_entry *register_class_Async_TimerEvent(zend_class_entry *class_entry_Async_CompletionPublisherAbstract, zend_class_entry *class_entry_Async_EventDescriptorInterface)
+{
+	zend_class_entry ce, *class_entry;
+
+	INIT_NS_CLASS_ENTRY(ce, "Async", "TimerEvent", class_Async_TimerEvent_methods);
+	class_entry = zend_register_internal_class_with_flags(&ce, class_entry_Async_CompletionPublisherAbstract, ZEND_ACC_FINAL);
+	zend_class_implements(class_entry, 1, class_entry_Async_EventDescriptorInterface);
+
+	return class_entry;
+}
+
+static zend_class_entry *register_class_Async_SignalEvent(zend_class_entry *class_entry_Async_CompletionPublisherAbstract, zend_class_entry *class_entry_Async_EventDescriptorInterface)
+{
+	zend_class_entry ce, *class_entry;
+
+	INIT_NS_CLASS_ENTRY(ce, "Async", "SignalEvent", class_Async_SignalEvent_methods);
+	class_entry = zend_register_internal_class_with_flags(&ce, class_entry_Async_CompletionPublisherAbstract, ZEND_ACC_FINAL);
+	zend_class_implements(class_entry, 1, class_entry_Async_EventDescriptorInterface);
+
+	return class_entry;
+}
+
+static zend_class_entry *register_class_Async_FileSystemEvent(zend_class_entry *class_entry_Async_CompletionPublisherAbstract, zend_class_entry *class_entry_Async_EventDescriptorInterface)
+{
+	zend_class_entry ce, *class_entry;
+
+	INIT_NS_CLASS_ENTRY(ce, "Async", "FileSystemEvent", class_Async_FileSystemEvent_methods);
+	class_entry = zend_register_internal_class_with_flags(&ce, class_entry_Async_CompletionPublisherAbstract, ZEND_ACC_FINAL);
+	zend_class_implements(class_entry, 1, class_entry_Async_EventDescriptorInterface);
+
+	return class_entry;
+}
+
+static zend_class_entry *register_class_Async_ProcessEvent(zend_class_entry *class_entry_Async_CompletionPublisherAbstract, zend_class_entry *class_entry_Async_EventDescriptorInterface)
+{
+	zend_class_entry ce, *class_entry;
+
+	INIT_NS_CLASS_ENTRY(ce, "Async", "ProcessEvent", class_Async_ProcessEvent_methods);
+	class_entry = zend_register_internal_class_with_flags(&ce, class_entry_Async_CompletionPublisherAbstract, ZEND_ACC_FINAL);
+	zend_class_implements(class_entry, 1, class_entry_Async_EventDescriptorInterface);
+
+	return class_entry;
+}
+
+static zend_class_entry *register_class_Async_ThreadEvent(zend_class_entry *class_entry_Async_CompletionPublisherAbstract, zend_class_entry *class_entry_Async_EventDescriptorInterface)
+{
+	zend_class_entry ce, *class_entry;
+
+	INIT_NS_CLASS_ENTRY(ce, "Async", "ThreadEvent", class_Async_ThreadEvent_methods);
+	class_entry = zend_register_internal_class_with_flags(&ce, class_entry_Async_CompletionPublisherAbstract, ZEND_ACC_FINAL);
+	zend_class_implements(class_entry, 1, class_entry_Async_EventDescriptorInterface);
 
 	return class_entry;
 }
