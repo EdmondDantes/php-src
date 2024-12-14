6e634982c538400f8ffe78d95aa00cd5785e7380
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-12-14 22:11:02 +0200

    + Chunked vector code.

--- async/internal/chunked_vector.h
+++ async/internal/chunked_vector.h
@@ -2,20 +2,39 @@
 #define CHUNKED_VECTOR_H
 
 
-typedef _chuncked_vector_s chuncked_vector_t;
-typedef _chunked_vector_chunk_s chuncked_vector_chunck_t;
+typedef _chunked_vector_s chunked_vector_t;
+typedef _chunked_vector_chunk_s chunked_vector_chunk_t;
 
-struct _chuncked_vector_s {
+struct _chunked_vector_s {
 	size_t item_size;
 	size_t initial_memory_size;
 	size_t trigger_chunk_size;
-	chuncked_vector_chunck_t *first;
+	int item_count;
+	int chunk_count;
+	chunked_vector_chunk_t *first;
 };
 
 struct _chunked_vector_chunk_s {
-	chuncked_vector_chunck_t* next;
+	chunked_vector_chunk_t* next;
 	size_t total;
 	size_t used;
 }
+
+#define CHUNKED_VECTOR_FOREACH(vector, type, item_var)                       \
+	chunked_vector_chunk_t* chunk = vector->first;						     \
+	size_t offset = 0;													     \
+	void* base_memory = (char*)chunk + sizeof(chunked_vector_chunk_t);	     \
+	while (chunk != NULL) { \
+		if (offset >= chunk->used) { \
+			chunk = chunk->next; \
+			offset = 0; \
+			continue; \
+		} \
+		type* item_var = base_memory + offset;
+
+
+#define CHUNKED_VECTOR_FOREACH_END() \
+		offset += vector->item_size; \
+	}
 
 #endif // CHUNKED_VECTOR_H
