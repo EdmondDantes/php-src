6e8ed261ef0b3ce5a9f90c39373a02240d7ae1e0
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-12-27 10:48:44 +0200

    + channel.c

---
+++ async/php_layer/channel.stub.php
@@ -0,0 +1,51 @@
+<?php
+
+/** @generate-class-entries */
+
+namespace Async;
+
+class ChannelException extends \Exception {}
+class ChannelWasClosed extends \Exception {}
+class ChannelIsFull extends \Exception {}
+
+/**
+ *
+ */
+class Channel
+{
+    public function __construct(int $capacity = 8, bool $expandable = false) {}
+
+    public function send(mixed $data, int $timeout = 0, Notifier $cancellation = null, bool $waitOnFull = true): void {}
+
+    public function sendAsync(mixed $data): void {}
+
+    public function receive(int $timeout = 0, Notifier $cancellation = null): mixed {}
+
+    public function receiveAsync(): mixed {}
+
+    public function close(): void {}
+
+    public function isClosed(): bool {}
+
+    public function isFull(): bool {}
+
+    public function isEmpty(): bool {}
+
+    public function isNotEmpty(): bool {}
+
+    public function getCapacity(): int {}
+
+    public function getUsed(): int {}
+}
+
+final class ThreadChannel extends Channel
+{
+    public function getSenderThreadId(): int {}
+    public function getReceiverThreadId(): int {}
+}
+
+final class ProcessChannel extends Channel
+{
+    public function getSenderProcessId(): int {}
+    public function getReceiverProcessId(): int {}
+}
