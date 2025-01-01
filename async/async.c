da47e13350a3112b6988b281d5ecfee3b3621ece
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2025-01-01 19:43:56 +0200

    + async_poll2

--- async/async.c
+++ async/async.c
@@ -22,6 +22,7 @@
 #include "php_reactor.h"
 #include "php_reactor.h"
 #include "php_scheduler.h"
+#include "libuv/libuv_reactor.h"
 #include "php_layer/functions.h"
 #include "php_layer/notifier.h"
 #include "php_layer/ev_handles.h"
@@ -408,9 +409,58 @@ void async_await_timeout(const zend_ulong timeout, reactor_notifier_t * cancella
 	GC_DELREF(&resume->std);
 }
 
-static zend_always_inline zend_ulong poll2_events_to_async(short events)
+static zend_always_inline zend_ulong poll2_events_to_async(const short events)
 {
+	zend_long result = 0;
 
+	if (events & POLLIN) {
+		result |= ASYNC_READABLE;
+	}
+
+	if (events & POLLOUT) {
+		result |= ASYNC_WRITABLE;
+	}
+
+	if (events & POLLHUP) {
+		result |= ASYNC_DISCONNECT;
+	}
+
+	if (events & POLLPRI) {
+		result |= ASYNC_PRIORITIZED;
+	}
+
+	if (events & POLLERR) {
+		result |= ASYNC_READABLE;
+	}
+
+	if (events & POLLNVAL) {
+		result |= ASYNC_READABLE;
+	}
+
+	return result;
+}
+
+static zend_always_inline short async_events_to_poll2(const zend_ulong events)
+{
+	short result = 0;
+
+	if (events & ASYNC_READABLE) {
+		result |= POLLIN;
+	}
+
+	if (events & ASYNC_WRITABLE) {
+		result |= POLLOUT;
+	}
+
+	if (events & ASYNC_DISCONNECT) {
+		result |= POLLHUP;
+	}
+
+	if (events & ASYNC_PRIORITIZED) {
+		result |= POLLPRI;
+	}
+
+	return result;
 }
 
 #define IF_EXCEPTION_GOTO_ERROR \
@@ -459,7 +509,25 @@ int async_poll2(php_pollfd *ufds, unsigned int nfds, const int timeout)
 
 	IF_EXCEPTION_GOTO_ERROR;
 
-	// TODO: code for calculation how many descriptors are ready
+	zval *notifier;
+	result = 0;
+
+	// calculation how many descriptors are ready
+	ZEND_HASH_FOREACH_VAL(resume->triggered_notifiers, notifier) {
+		if (Z_TYPE_P(notifier) == IS_OBJECT && instanceof_function(Z_OBJ_P(notifier)->ce, async_ce_ev_handle)) {
+			result++;
+
+			const php_socket_t socket = ((libuv_poll_t *) Z_OBJ_P(notifier))->uv_handle->socket;
+
+			// Fine the same socket in the ufds array
+			for (unsigned int i = 0; i < nfds; i++) {
+				if (ufds[i].fd == socket) {
+					ufds[i].revents = async_events_to_poll2(Z_LVAL_P(async_ev_handle_get_triggered_events(Z_OBJ_P(notifier))));
+					break;
+				}
+			}
+		}
+	} ZEND_HASH_FOREACH_END();
 
 finally:
 
